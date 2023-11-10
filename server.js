12a8a7ffe064da35ef9732bcb4d0a17e3932d349
Ftv123 <116049632+Ftv123@users.noreply.github.com>
2023-11-10 04:25:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const io = require('socket.io')(http);
 
 // Configuración del servidor de Socket.io
 io.on('connection', (socket) => {
-    // Tu lógica de manejo de conexiones de clientes aquí
+    // Lógica de manejo de conexiones de clientes aquí
+    socket.on('offerFromOtherUser', (offer) => {
+        // Aquí generarías la respuesta del otro usuario
+        const respuesta = '¡Esta es la respuesta del otro usuario!';
+
+        // Emitir la respuesta al usuario que envió la oferta
+        socket.emit('answerFromOtherUser', respuesta);
+    });
 });
 
 http.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
 });
 
-
-
-
-    </script>
-</body>
-</html>
